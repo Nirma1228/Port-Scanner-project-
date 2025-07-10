f78661f622cc5876fac2df53b74ee5a76b770198
Nirma Nethmini <nirmanethmini@gmail.com>
2025-07-10 19:05:57 +0530

    Update PortScanner.java

--- PortScanner.java
+++ PortScanner.java
@@ -2,3 +2,8 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.InetSocketAddress;
 import java.util.Scanner;
+
+public class PortScanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
